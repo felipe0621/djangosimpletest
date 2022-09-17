aa81965c8fb3c476d284fb533a52973a7e2dff30
pipe <pipe@gmail.com>
2022-09-16 20:57:42 -0500

    conectando a heroku

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -110,11 +110,22 @@ WSGI_APPLICATION = 'drfsimplecrud.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+'''
 DATABASES = { 
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
         conn_max_age=600 )
+ }
+ '''
+
+DATABASES = { 
+    'default': { 'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+    'NAME': 'dbbuqjb4toh4rn',
+    'USER': 'vujypjvkqopvik', 
+    'PASSWORD': '210c8b75307b5e32e066133377b4ed894a823aefc2ddb370bfddd48b2f81f17e', 
+    'HOST': 'ec2-44-209-158-64.compute-1.amazonaws.com',
+    'PORT': '5432', 
+    }
  }
 
 
